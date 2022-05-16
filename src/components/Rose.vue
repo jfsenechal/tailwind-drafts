af98d6d0473ff89f9ba9f71b0858e15f2f143258
jfsenechal <jfsenechal@gmail.com>
2022-05-16 20:10:03 +0200

    essais fly

--- src/components/Rose.vue
+++ src/components/Rose.vue
@@ -28,18 +28,6 @@
     <main>
       <!-- CTA section -->
       <div class="relative mt-24 sm:mt-32 sm:py-16">
-        <div aria-hidden="true" class="hidden sm:block">
-          <div class="absolute inset-y-0 left-0 w-1/2 bg-gray-50 rounded-r-3xl" />
-          <svg class="absolute top-8 left-1/2 -ml-3" width="404" height="392" fill="none" viewBox="0 0 404 392">
-            <defs>
-              <pattern id="8228f071-bcee-4ec8-905a-2a059a2cc4fb" x="0" y="0" width="20" height="20"
-                       patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width="404" height="392" fill="url(#8228f071-bcee-4ec8-905a-2a059a2cc4fb)" />
-          </svg>
-        </div>
         <div class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
           <div class="relative rounded-2xl px-6 py-10 bg-rose-500 overflow-hidden shadow-xl sm:px-12 sm:py-20">
             <div aria-hidden="true" class="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0">
