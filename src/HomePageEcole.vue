aea2872aa6b51db9cd63fcbe8fefe614d4565c89
jfsenechal <jfsenechal@gmail.com>
2022-05-19 21:16:15 +0200

    homepage ecole

--- src/HomePageEcole.vue
+++ src/HomePageEcole.vue
@@ -63,12 +63,6 @@ const callback = (isOpen) => openSearch.value = isOpen;
         </div>
       </div>
     </div>
-    <!--
-     <div class="pt-4 pb-20 px-4 sm:px-6 lg:pt-4 lg:pb-28 lg:px-8">
-       <div class="max-w-7xl mx-auto">
-
-       </div>
-     </div>-->
     <Footer />
   </div>
 </template>
